98009085e8f114d209c1180aa6217d5348da1fb1
Mikel Duffy <mduffy23@gmail.com>
2016-11-30 23:02:08 -0800

    Updates custom.zsh

---
+++ custom.zsh
@@ -0,0 +1,7 @@
+ZSH_THEME="gallois"
+ENABLE_CORRECTION="true"
+COMPLETION_WAITING_DOTS="true"
+plugins=(history tmux zsh-autosuggestions npm git colored-man colorize zsh-syntax-highlighting)
+
+alias vim=nvim
+alias c="clear"
